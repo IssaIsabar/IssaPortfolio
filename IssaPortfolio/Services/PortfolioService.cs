f10170b2416829c5918997b363b686a6c2940fd9
Issa <issa.isabar@hotmail.com>
2022-05-17 10:37:47 +0200

    ui changes

--- IssaPortfolio/Services/PortfolioService.cs
+++ IssaPortfolio/Services/PortfolioService.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Text;
 using System.Text.Json;
 
 namespace IssaPortfolio.Services.PortfolioService
@@ -49,30 +50,23 @@ namespace IssaPortfolio.Services.PortfolioService
                 return JsonSerializer.Deserialize<List<PortfolioItem>>(body, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
             }
         }
-        //public async Task<List<PortfolioItem>?> LoadPortfolioItems()
-        //{
-        //    try
-        //    {
-        //        using (var client = new HttpClient())
-        //        {
-        //            return await client.GetFromJsonAsync<List<PortfolioItem>>("https://localhost:7142/api/PortfolioItem/getitems");
-        //        }
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Console.WriteLine("Message :{0} ", e.Message);
-        //    }
-        //    return null;
-        //}
-        public async Task AddPortfolioItem(string name, string desc, string imgUrl)
-        {
-            if (!string.IsNullOrWhiteSpace(name) && !string.IsNullOrWhiteSpace(desc) && !string.IsNullOrWhiteSpace(imgUrl))
-            {
-                var item = new PortfolioItem(name, desc, imgUrl);
 
-                using (var _http = new HttpClient())
+        public async Task AddPortfolioItem(PortfolioItem portItem)
+        {
+            string jsonString = JsonSerializer.Serialize(portItem);
+            if (!string.IsNullOrWhiteSpace(portItem.Name) && !string.IsNullOrWhiteSpace(portItem.Description) && !string.IsNullOrWhiteSpace(portItem.ImageUrl))
+            {
+                var request = new HttpRequestMessage
                 {
-                    await _http.PostAsJsonAsync("https://localhost:7142/api/PortfolioItem/postportfolio", item);
+                    Method = HttpMethod.Post,
+                    RequestUri = GetUri("/api/PortfolioItem/postportfolio"),
+                    Content = new StringContent(jsonString, Encoding.UTF8, "application/json")
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(body);
                 }
             }
         }
@@ -81,7 +75,7 @@ namespace IssaPortfolio.Services.PortfolioService
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Delete,
-                RequestUri = new Uri("https://localhost:44316/api/PortfolioItem/deletePortfolio/" + id),
+                RequestUri = GetUri("/api/PortfolioItem/deletePortfolio/" + id),
             };
             using (var response = await client.SendAsync(request))
             {
